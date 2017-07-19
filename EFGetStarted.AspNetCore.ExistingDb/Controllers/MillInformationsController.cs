5a8a8040c100868b087d54735c1b424ef008ee45
Aditya Gune <adi.vtca@gmail.com>
2017-07-19 15:49:13 -0700

    fixed scoping

--- EFGetStarted.AspNetCore.ExistingDb/Controllers/MillInformationsController.cs
+++ EFGetStarted.AspNetCore.ExistingDb/Controllers/MillInformationsController.cs
@@ -162,31 +162,4 @@ namespace EFGetStarted.AspNetCore.ExistingDb.Controllers
             return _context.MillInformation.Any(e => e.PkMillKey == id);
         }
     }
-
-    
-
-    public class MillSearchLogic
-    {
-        private MillDataContext MillContext;
-        private DbContextOptions<MillDataContext> options;
-
-        public MillSearchLogic()
-        {
-            MillContext = new MillDataContext(options);
-        }
-
-        public IQueryable<MillInformation> SearchMill(MillSearchModel searchModel)
-        {
-            var result = MillContext.MillInformation.AsQueryable();
-
-            if(searchModel != null)
-            {
-                result = result.Where(x => x.MillId == searchModel.MillId);
-            }
-
-
-            return result;
-        }
-    }
-
 }
--- EFGetStarted.AspNetCore.ExistingDb/Models/MillSearchModel.cs
+++ EFGetStarted.AspNetCore.ExistingDb/Models/MillSearchModel.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using EFGetStarted.AspNetCore.ExistingDb.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace EFGetStarted.AspNetCore.ExistingDb.Controllers
+namespace EFGetStarted.AspNetCore.ExistingDb.Models
 {
     public class MillSearchModel
     {
