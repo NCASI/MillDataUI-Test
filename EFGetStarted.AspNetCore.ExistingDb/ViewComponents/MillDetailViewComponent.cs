aba11e50b3b163ddfe3f1852c8c75aafadbce252
Aditya Gune <adi.vtca@gmail.com>
2017-07-26 15:16:32 -0700

    added test view component

---
+++ EFGetStarted.AspNetCore.ExistingDb/ViewComponents/MillDetailViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MillData.Models;
+
+namespace ViewComponentSample.ViewComponents
+{
+    public class MillDetailViewComponent : ViewComponent
+    {
+        private readonly MillDataContext db;
+
+        public MillDetailViewComponent(MillDataContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(
+        int maxPriority, bool isDone)
+        {
+            var items = await GetItemsAsync(maxPriority, isDone);
+            return View(items);
+        }
+        private Task<List<MillInformation>> GetItemsAsync(int maxPriority, bool isDone)
+        {
+            //return db.MillInformation.Where(x => x.IsDone == isDone &&
+            //                     x.Priority <= maxPriority).ToListAsync();
+        }
+    }
+}
