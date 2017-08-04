9c13e5d900a0309f02d142aa903c650a0b28bf76
Aditya Gune <adi.vtca@gmail.com>
2017-08-03 18:48:22 -0700

    experimenting with ProdData views

--- EFGetStarted.AspNetCore.ExistingDb/ViewComponents/MillDetail.cs
+++ EFGetStarted.AspNetCore.ExistingDb/ViewComponents/MillDetail.cs
@@ -29,7 +29,19 @@ namespace MillData.ViewComponents
 
             var result = db.MillInformation.Where(x => x.MillId == id).ToListAsync();
 
+            
+
             return result;
+        }
+
+        public async Task<IActionResult> GetProdData(int? id)
+        {
+            var prodData = await db.ProductionData
+                .Include(m => m.FkProdCat.ProdType)
+                .Include(m => m.FkProdCat.Category)
+                .SingleOrDefaultAsync(m => m.FkMillKey == id);
+
+            return ViewData["ProdData"] = prodData;
         }
     }
 }
