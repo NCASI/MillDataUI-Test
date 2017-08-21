30cd53a23df73968da4134392927fdecda5b71f3
Aditya Gune <adi.vtca@gmail.com>
2017-08-21 16:26:44 -0700

    changing display name for production data

--- EFGetStarted.AspNetCore.ExistingDb/Models/ProductionData.cs
+++ EFGetStarted.AspNetCore.ExistingDb/Models/ProductionData.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace MillData.Models
 {
     public partial class ProductionData
     {
         public int PkProductionId { get; set; }
+
+        [DisplayName("Mill ID")]
         public int? FkMillKey { get; set; }
+
+        [DisplayName("Production Category")]
         public int? FkProdCatId { get; set; }
         public float? Quantity { get; set; }
         public string Units { get; set; }
+
+        [DisplayName("Source")]
         public int? FkSourceId { get; set; }
+
+        [DisplayName("Year")]
         public int? SourceYear { get; set; }
         public string Comments { get; set; }
 
